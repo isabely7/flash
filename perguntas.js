b974cb27c70cf29c1999b4690aee31ce32908ac5
isabely7 <166526171+isabely7@users.noreply.github.com>
2024-10-22 10:44:54 -0300

    22/10

---
+++ perguntas.js
@@ -0,0 +1,41 @@
+criaCartao (
+    'Suco',
+    'O suco de vocês é natural?',
+    'Sim.'
+
+)
+
+criaCartao (
+    'Suco',
+    'Vocês fazem entrega?',
+    'Sim.'
+
+)
+
+criaCartao (
+    'Suco',
+    'Qual o melhor suco de Londrina?',
+    'O do Point do Suco.'
+
+)
+
+criaCartao (
+    'Suco',
+    'O quais sabores vocẽs tem?',
+    'Temos laranja, maracujá, uva, goiaba, laranja com morango.'
+
+)
+
+criaCartao (
+    'Suco',
+    'Quanto custa o litro?',
+    '1l 13,00 - 2l 24,00 - 3l 33,00 - 5l 54,00.'
+
+)
+
+criaCartao (
+    'Suco',
+    'Vocês vendem outras coisas?',
+    'Sim.'
+
+)
