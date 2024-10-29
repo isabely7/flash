8fbff8b365ae58f59c80473469112a9dfae9de72
isabely7 <166526171+isabely7@users.noreply.github.com>
2024-10-29 11:26:23 -0300

    29

--- perguntas.js
+++ perguntas.js
@@ -1,41 +1,44 @@
 criaCartao (
-    'Suco',
-    'O suco de vocês é natural?',
-    'Sim.'
+    'O que é o que é?',
+    'Eu sou seu irmão, mas você não é meu irmão. Como será que isso é possível??',
+    'Você não é meu irmÃO, porque é minha irmÃ.'
 
 )
 
 criaCartao (
-    'Suco',
-    'Vocês fazem entrega?',
-    'Sim.'
+    'O que é, o que é?',
+   'Tem pescoço e não tem cabeça, tem braços e não tem mãos, tem corpo e não tem pernas.',
+   'A camisa.'
 
 )
 
 criaCartao (
-    'Suco',
-    'Qual o melhor suco de Londrina?',
-    'O do Point do Suco.'
+    'O que é o que é?',
+    'O que sobe uma chaminé para baixo, mas não desce uma chaminé para cima?',
+    'Um guarda-chuva. Porque para baixo, ou seja, fechado, ele cabe na chaminé. O guarda-chuva para cima, ou seja, aberto, não cabe.'
+
+)
+
+
+criaCartao (
+    'O que é o que é?',
+   ' Fica no início da rua, no fim do mar e no meio da cara.',
+   'A letra "r"'
 
 )
 
 criaCartao (
-    'Suco',
-    'O quais sabores vocẽs tem?',
-    'Temos laranja, maracujá, uva, goiaba, laranja com morango.'
+     'O que é o que é?',
+    'Se o dia anterior a ontem é o dia 21, que dia é o dia depois de amanhã?',
+    'Dia 25. Ontem foi 22, anteontem foi 21, hoje é o dia 23, amanhã é o dia 24 e depois de amanhã é o dia 25.'
 
 )
 
 criaCartao (
-    'Suco',
-    'Quanto custa o litro?',
-    '1l 13,00 - 2l 24,00 - 3l 33,00 - 5l 54,00.'
+     'O que é o que é?',
+    'Tem centenas de rodas, mas não sai do lugar.',
+    'O estacionamento.'
 
 )
 
-criaCartao (
-    'Suco',
-    'Vocês vendem outras coisas?',
-    'Sim.'
 
-)
